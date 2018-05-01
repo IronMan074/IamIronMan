323c9b392a5e54d01ed71863e7dde3dddff2fef9
JARVIS <JARVIS@JARVIS-HP>
2018-05-01 21:35:46 +0200

    Add project files.

---
+++ WebApplication39/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication39.Startup))]
+namespace WebApplication39
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
